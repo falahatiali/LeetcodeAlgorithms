828b8390cdc17f4ce3df68db02a65ae534e1acbc
Ali Agho <ali_fm2009@yahoo.com>
2022-11-04 14:26:12 +0100

    Two Sum problem was solved

---
+++ README.md
@@ -0,0 +1,1 @@
+# LettCode_Algorithm_With_PHP_EASY_TO_HARD
---
+++ src/TwoSum/Solution.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+/**
+ * RESULT AFTER SUBMISSION
+ *
+ * 57 / 57 test cases passed
+ * Status: Accepted
+ * Runtime: 495 ms
+ * Memory Usage: 19.9 MB
+ *
+ */
+class Solution
+{
+    public function twoSum($nums, $target)
+    {
+        $res = [];
+        for ($i = 0; $i < count($nums); $i++) {
+            $diff = $target - $nums[$i];
+
+            if (in_array($diff, $this->diffArray($nums, $i))) {
+                $res[] = $i;
+                $res[] = array_search($diff, $this->diffArray($nums, $i));
+                break;
+            }
+        }
+        return $res;
+    }
+
+    private function diffArray($nums, $i)
+    {
+        unset($nums[$i]);
+        return $nums;
+    }
+}
