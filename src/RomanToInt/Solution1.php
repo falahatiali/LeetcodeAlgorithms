261317aebc0be225fa8df47f1e2765be4d861653
Ali Agho <ali_fm2009@yahoo.com>
2022-11-08 10:55:46 +0100

    Roman To Integer problem solved

---
+++ src/RomanToInt/Solution1.php
@@ -0,0 +1,40 @@
+<?php
+
+class Solution1
+{
+    public function romanToInteger($s){
+        $data = [
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000
+        ];
+        if (in_array($s, $data)) {
+            return $data[$s];
+        }
+
+        $sum = 0;
+        $key = 0;
+
+        while ($key < strlen($s)) {
+            $currentWordValue = $data[$s[$key]];
+            if ($key < strlen($s) - 1) {
+                if ($data[$s[$key + 1]] > $currentWordValue) {
+                    $sum += $data[$s[$key + 1]] - $currentWordValue;
+                    $key = $key + 2;
+                } else {
+                    $sum += $data[$s[$key]];
+                    $key++;
+                }
+            } else {
+                $sum += $data[$s[$key]];
+                $key++;
+            }
+        }
+
+        return $sum;
+    }
+}
